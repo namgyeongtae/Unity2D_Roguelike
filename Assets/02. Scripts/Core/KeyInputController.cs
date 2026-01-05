cd56e5d776752123ef3008fdf4457b9cd9d9ed48
namgyeongtae <gotsuebel@gmail.com>
2026-01-05 11:23:47 +0900

    Fix KeyInput System

--- Assets/02. Scripts/Core/KeyInputController.cs
+++ Assets/02. Scripts/Core/KeyInputController.cs
@@ -1,60 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum ActionId
+{
+    MoveUp, MoveDown, MoveLeft, MoveRight,
+    Attack, Dash, Interact
+}
+
+public enum InputPhase { Down, Up, Hold }
 
 public class KeyInputController : MonoBehaviour
 {
-    public delegate void KeyInputHandler(Vector2 inputDir);
-
     private static KeyInputController _instance;
+
     public static KeyInputController Instance
     {
         get
         {
-            if (_instance == null)
-            {
-                _instance = FindFirstObjectByType<KeyInputController>();
-            }
+            if (_instance == null) _instance = FindFirstObjectByType<KeyInputController>();
             return _instance;
         }
     }
 
+    private readonly Dictionary<(ActionId, InputPhase), Action> _commands = new();
+    private readonly Dictionary<(ActionId, InputPhase), Action<Vector2>> _directionCommands = new();
 
-    public event KeyInputHandler onKeyInputDown;
-    private bool _isKeyDown = false;
+    private readonly Dictionary<ActionId, KeyCode> _bindings = new();
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private readonly HashSet<KeyCode> _usedKeys = new();
+
+    public event Action<Vector2> onDirectionInput;
+
+    public void Bind(ActionId actionId, KeyCode key)
     {
-        
+        if (_bindings.TryGetValue(actionId, out var existingKey))
+        {
+            if (existingKey == key)
+                return;
+            
+            _usedKeys.Remove(existingKey);
+        }
+
+        _bindings[actionId] = key;
+        _usedKeys.Add(key);
     }
 
-    // Update is called once per frame
-    void Update()
+    public KeyCode GetBinding(ActionId actionId) => _bindings[actionId];
+
+    public void SetCommand(ActionId actionId, InputPhase phase, Action command)
     {
-       OnKeyDown();
-       OnKeyUp();
+        _commands[(actionId, phase)] = command;
     }
 
-    private void OnKeyDown()
+    /* public void SetCommand(ActionId actionId, InputPhase phase, Action<Vector2> command)
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-            onKeyInputDown?.Invoke(Vector2.up);
-        else if (Input.GetKey(KeyCode.LeftArrow))
-            onKeyInputDown?.Invoke(Vector2.left);
-        else if (Input.GetKey(KeyCode.DownArrow))
-            onKeyInputDown?.Invoke(Vector2.down);
-        else if (Input.GetKey(KeyCode.RightArrow))
-            onKeyInputDown?.Invoke(Vector2.right);
+        _directionCommands[(actionId, phase)] = command;
+    } */
+
+    private void Awake()
+    {
+        Bind(ActionId.MoveUp, KeyCode.UpArrow);
+        Bind(ActionId.MoveDown, KeyCode.DownArrow);
+        Bind(ActionId.MoveLeft, KeyCode.LeftArrow);
+        Bind(ActionId.MoveRight, KeyCode.RightArrow);
+
+        Bind(ActionId.Attack, KeyCode.LeftControl);
+        Bind(ActionId.Dash, KeyCode.LeftShift);
+        Bind(ActionId.Interact, KeyCode.F);
     }
 
-    private void OnKeyUp()
+    private void Start()
     {
-        //if (!_isKeyDown)
-        //    return;
+        SetCommand(ActionId.Attack, InputPhase.Down, () => Debug.Log("Attack"));
+        SetCommand(ActionId.Dash, InputPhase.Down, () => Debug.Log("Dash"));
+        SetCommand(ActionId.Interact, InputPhase.Down, () => Debug.Log("Interact"));
 
-        //if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.LeftArrow) 
-        //|| Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow))
-        //{
-        //    _isKeyDown = false;
-        //}
+        SetCommand(ActionId.MoveUp, InputPhase.Hold, () => onDirectionInput?.Invoke(Vector2.up));
+        SetCommand(ActionId.MoveDown, InputPhase.Hold, () => onDirectionInput?.Invoke(Vector2.down));
+        SetCommand(ActionId.MoveLeft, InputPhase.Hold, () => onDirectionInput?.Invoke(Vector2.left));
+        SetCommand(ActionId.MoveRight, InputPhase.Hold, () => onDirectionInput?.Invoke(Vector2.right));
+
+        SetCommand(ActionId.MoveUp, InputPhase.Up, () => onDirectionInput?.Invoke(Vector2.zero));
+        SetCommand(ActionId.MoveDown, InputPhase.Up, () => onDirectionInput?.Invoke(Vector2.zero));
+        SetCommand(ActionId.MoveLeft, InputPhase.Up, () => onDirectionInput?.Invoke(Vector2.zero));
+        SetCommand(ActionId.MoveRight, InputPhase.Up, () => onDirectionInput?.Invoke(Vector2.zero));
+    }
+
+    private void Update()
+    {
+        foreach (var key in _usedKeys)
+        {
+            if (Input.GetKeyDown(key)) InvokeByKey(key, InputPhase.Down);
+            if (Input.GetKeyUp(key)) InvokeByKey(key, InputPhase.Up);
+            if (Input.GetKey(key)) InvokeByKey(key, InputPhase.Hold);
+        }
+    }
+
+    private void InvokeByKey(KeyCode key, InputPhase phase)
+    {
+        foreach (var pair in _bindings)
+        {
+            if (pair.Value != key) continue;
+
+            if (_commands.TryGetValue((pair.Key, phase), out var command))
+                command?.Invoke();
+
+            /* if (_directionCommands.TryGetValue((pair.Key, phase), out var directionCommand))
+                directionCommand?.Invoke(Vector2.zero); */
+            
+            break;
+        }
     }
 }
