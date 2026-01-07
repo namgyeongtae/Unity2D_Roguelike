a0cf79b5ff97e7f78988c8e2556dd0a21fec0d32
namgyeongtae <gotsuebel@gmail.com>
2026-01-07 09:24:07 +0900

    Add Camera Follow

---
+++ Assets/02. Scripts/Core/CameraFollow.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private const float MIN_CLAMP_X = -9;
+    private const float MAX_CLAMP_X = 32;
+    private const float MIN_CLAMP_Y = -8.5f;
+    private const float MAX_CLAMP_Y = 6.5f;
+
+    [SerializeField] private Transform followTarget;
+    [SerializeField] private Vector3 offset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowTarget();
+    }
+
+    void FollowTarget()
+    {
+        Vector3 targetPos = followTarget.position + offset;
+
+        targetPos.x = Mathf.Clamp(targetPos.x, MIN_CLAMP_X, MAX_CLAMP_X);
+        targetPos.y = Mathf.Clamp(targetPos.y, MIN_CLAMP_Y, MAX_CLAMP_Y);
+
+        transform.position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
+    }
+}
